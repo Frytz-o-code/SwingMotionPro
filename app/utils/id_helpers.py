2d06a83510c108b90eae81d08581a44a04b89cb6
Frytz-o-code <129654302+Frytz-o-code@users.noreply.github.com>
2025-06-15 11:38:57 +0000

    initial commit

--- app/utils/id_helpers.py
+++ app/utils/id_helpers.py
@@ -5,6 +5,6 @@ def make_id_factory(prefix: str):
     clean_prefix = re.sub(r'\W+', '-', prefix).strip('-')
     def _id(name: str):
         full_id = f"{clean_prefix}-{name}"
-        print(f"[ID_FACTORY] Generated ID: {full_id}")
+        #print(f"[ID_FACTORY] Generated ID: {full_id}")
         return full_id
     return _id
